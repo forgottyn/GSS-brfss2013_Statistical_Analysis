860ebb1192c60322738eb1798157123189501c4b
forgottyn <43021964+forgottyn@users.noreply.github.com>
2019-07-05 14:00:53 -0700

    Update statistical_analysis_code.R

--- statistical_analysis_code.R
+++ statistical_analysis_code.R
@@ -1,5 +1,6 @@
 library(ggplot2)
 library(dplyr)
+library(statsr)
 
 load("brfss2013.RData")
 
@@ -26,3 +27,23 @@ ggplot(women_adt_veg, na.rm = TRUE,  aes(x =fvgreen , fill = X_incomg)) +
        x = "Dark Green Vegetables",
        y = "Number of Women") +
   scale_fill_manual(values = c("#ffffcc", "#a1dab4", "#41b6c4", "#2c7fb8", '#253494')) 
+
+load("gss.Rdata")
+
+#prepare required data for analyzing and draw plot
+ggplot(gss, aes(x = degree, fill = sex)) +
+  geom_histogram(stat = "count",  position="dodge") +
+  labs(title = "Education Status Distribution with Gender",
+       x = "Degree Level",
+       y = "Number of People") +
+  guides(fill = guide_legend(label = TRUE)) + 
+  theme_minimal()+
+  scale_fill_manual(values = c("#f5f5f5", "#5ab4ac"))
+
+#take hypothesis test
+inference(y=sex, x= degree, 
+          data = filter(gss, !is.na(degree)), 
+          type = "ht", statistic = "proportion", 
+          success = c("Bachelor","Master"), 
+          method = "theoretical", alternative = "greater",
+          sig_level = 0.05, conf_level = 0.95)
